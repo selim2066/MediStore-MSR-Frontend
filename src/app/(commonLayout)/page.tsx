f600377a5026764482501da850c8522ea5ed32ba
selim2066 <mdselimreza2066@gmail.com>
2026-04-29 19:04:00 +0600

    bug fixed. remove feturedSection.

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,6 +1,6 @@
 import { CategoriesSection } from "@/components/module/home/categories-section"
 import { FeaturedMedicinesSection } from "@/components/module/home/featured-medicines-section"
-import FeatureSection from "@/components/module/home/FeatureSection"
+
 import { HeroSection } from "@/components/module/home/hero-section"
 import { TestimonialsSection } from "@/components/module/home/testimonials-section"
 import { WhyUsSection } from "@/components/module/home/why-us-section"
