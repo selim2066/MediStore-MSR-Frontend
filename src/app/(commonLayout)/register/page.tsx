50c4448823e0aa4f174e944f70fc902cfd00b41c
selim2066 <mdselimreza2066@gmail.com>
2026-04-29 19:06:42 +0600

    bug fiexed: registration form

--- src/app/(commonLayout)/register/page.tsx
+++ src/app/(commonLayout)/register/page.tsx
@@ -4,7 +4,7 @@ import { RegisterForm } from "@/components/module/authentication/registration";
 export default function RegisterPage() {
   return (
     <div className="min-h-screen flex items-center justify-center px-4 py-5">
-      <RegisterForm className="w-full max-w-md" />
+      <RegisterForm/>
     </div>
   )
 }
