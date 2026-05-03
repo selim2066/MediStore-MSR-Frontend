d486ddc2f29f508e27218239c20a6f61210264c4
selim2066 <mdselimreza2066@gmail.com>
2026-05-03 21:04:38 +0600

    bug fixed test :007

--- src/app/(customerLayout)/layout.tsx
+++ src/app/(customerLayout)/layout.tsx
@@ -1,12 +1,16 @@
 import { Footer } from "@/components/layout/footer"
 import { Navbar } from "@/components/layout/navbar1"
+import { userService } from "@/service/user.service"
+import { redirect } from "next/navigation"
+import { headers } from "next/headers"
 
+export default async function CustomerLayout({ children }: { children: React.ReactNode }) {
+  const headersList = await headers()
+  const cookieHeader = headersList.get("cookie") || ""
 
-export default function CustomerLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+  const { data: session } = await userService.getSession(cookieHeader)
+  if (!session?.user) redirect("/login")
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
