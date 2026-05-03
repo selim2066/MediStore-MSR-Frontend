60b928a0b359f50cc3bf1cb0c61405da9f912912
selim2066 <mdselimreza2066@gmail.com>
2026-05-03 17:13:04 +0600

    bug test 005 locally works

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,9 @@
 import { createAuthClient } from "better-auth/react"
 import { inferAdditionalFields } from "better-auth/client/plugins"
+import { env } from "@/env"
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: env.NEXT_PUBLIC_BACKEND_URL,
   plugins: [
     inferAdditionalFields({
       user: {
